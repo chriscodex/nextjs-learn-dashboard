fd89d10164a4d1b23a17458546124acdd1944a34
Christian <c.espinoza.code@gmail.com>
2024-07-14 15:50:51 -0500

    sidenav now its a client component

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,4 +1,6 @@
+'use client';
 import Link from 'next/link';
+
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
